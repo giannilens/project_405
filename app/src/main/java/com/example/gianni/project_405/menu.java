39db4226e99c9bb6f678a0cae3bc3005beeda395
Gianni Lens <gianni-lens@hotmail.com>
2018-02-23 16:15:19 +0100

    added files

--- app/src/main/java/com/example/gianni/project_405/menu.java
+++ app/src/main/java/com/example/gianni/project_405/menu.java
@@ -2,12 +2,38 @@ package com.example.gianni.project_405;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class menu extends AppCompatActivity {
+public class menu extends AppCompatActivity implements OnClickListener {
 
     @Override
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+        scanBtn.setOnClickListener(this);
     }
 }
+
+    public void onClick(View v){
+    //respond to clicks
+        if(v.getId()==R.id.scan_button) {
+            //scan
+        }
+        IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+        scanIntegrator.initiateScan();
+    }
+
