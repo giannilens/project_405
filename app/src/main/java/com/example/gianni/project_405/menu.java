b114ac7db23df29d4860e1bb2f005df0c70590e0
ffarys <frederik.arys@telenet.be>
2018-03-27 10:32:59 +0200

    login complete + "in app" login button broke

--- app/src/main/java/com/example/gianni/project_405/menu.java
+++ app/src/main/java/com/example/gianni/project_405/menu.java
@@ -2,20 +2,18 @@ package com.example.gianni.project_405;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.gianni.project_405.activities.login_activity;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.EditText;
 
 public class menu extends AppCompatActivity implements OnClickListener {
 
